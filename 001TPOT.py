872742a685cc461b0ea5b2d5f8430d1655ffd236
Shan Zhu <39196979+Shan-Zhu@users.noreply.github.com>
2023-06-20 18:46:27 +0800

    Add files via upload

---
+++ 001TPOT.py
@@ -0,0 +1,25 @@
+
+#coding: utf-8
+
+'''
+TPOT
+'''
+
+import pandas as pd
+from tpot import TPOTRegressor
+from sklearn.model_selection import train_test_split
+
+
+# input data
+data_input=pd.read_csv('D:/2022-Paper-LC-Alloy/20230423-alloy-HER-comp.csv', sep=',')
+
+labels=data_input['Overpotential (mV)']
+features=data_input.drop('Overpotential (mV)', axis=1).drop('DOI', axis=1).drop('Composition', axis=1)
+
+X_train,X_test,y_train,y_test=train_test_split(features, labels, test_size=0.2, random_state=42)
+
+tpot = TPOTRegressor(generations=100, random_state=0,verbosity=2,template='Regressor')
+
+tpot.fit(X_train, y_train)
+print(tpot.score(X_test, y_test))
+tpot.export('ML_Alloy_Carbon_TPOT_pipeline_comp2.py')
