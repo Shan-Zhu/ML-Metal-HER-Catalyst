c1f56c8a7d5d6ebc55d3ffbe40e43814dab60a5d
Shan Zhu <39196979+Shan-Zhu@users.noreply.github.com>
2023-06-21 08:44:53 +0800

    Update 003SHAP.py

--- 003SHAP.py
+++ 003SHAP.py
@@ -25,7 +25,7 @@ plt.rcParams["font.family"] = "Arial"
 plt.rcParams["font.size"] = 12
 my_cmap = plt.cm.get_cmap('RdBu').reversed()
 
-tpot_data = pd.read_csv('D:/2022-Paper-LC-Alloy/20230423-alloy-HER-comp-noDOI.csv', sep=',', dtype=np.float64)
+tpot_data = pd.read_csv('.....csv', sep=',', dtype=np.float64)
 features = tpot_data.drop('Overpotential (mV)', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['Overpotential (mV)'], random_state=42)
@@ -50,27 +50,10 @@ predictt = exported_pipeline.predict(training_features)
 print(predictt.mean())
 
 shap_values = explainer.shap_values(features)
-np.savetxt("D:/2022-Paper-LC-Alloy/SHAP-data-test.csv", shap_values, delimiter=",")
-
-# Set font family and size for x-axis label
-# plt.xlabel('X Label', fontfamily='Arial', fontsize=12)
-# # Set font family and size for y-axis label
-# plt.ylabel('Y Label', fontfamily='Arial', fontsize=12)
-# Set font family and size for tick labels on both axes
+np.savetxt(".....csv", shap_values, delimiter=",")
 
 fig = plt.figure() #figsize=(8, 4)
 shap.summary_plot(shap_values, features, show=False, cmap=my_cmap)
-# fig2=shap.summary_plot(shap_values, features, plot_type="bar", cmap=my_cmap) #加和平均
-# shap.dependence_plot("N_doped (at%)", shap_values, features,cmap=my_cmap)
-
-# shap_interaction_values = explainer.shap_interaction_values(features)
-# shap.summary_plot(shap_interaction_values, features)
-
-
-fig.set_size_inches(6, 5.3)
-plt.xticks(fontfamily='Arial', fontsize=12)
-plt.yticks(fontfamily='Arial', fontsize=12)
-plt.savefig('D:/2022-Paper-LC-Alloy/Figure/Figure1c.tiff',dpi=600,format='tiff',bbox_inches='tight')
 
 plt.show()
 
